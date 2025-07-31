e22d1ae7f6039c0ffadf35c4e869ea706070b0ec
naveenrnk66 <naveenramayanapu66.r@gmail.com>
2025-07-31 18:41:21 +0530

    first commt

--- vite.config.ts
+++ vite.config.ts
@@ -4,9 +4,10 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/rnk-portfolio/',
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
   // ADD THIS LINE FOR DEPLOYMENT TO GITHUB PAGES OR SIMILAR SUBPATH HOSTING
-  base: '/portfolio/', // <--- IMPORTANT: Replace 'portfolio' with your actual GitHub repository name
+   // <--- IMPORTANT: Replace 'portfolio' with your actual GitHub repository name
 });
