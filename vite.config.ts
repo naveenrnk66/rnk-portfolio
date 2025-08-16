6ecb5817a9e6e9ff8603d9f8e58220dc19cfb2c2
naveenrnk66 <naveenramayanapu66.r@gmail.com>
2025-08-16 16:05:59 +0530

    Updated portfolio

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,18 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   base: '/rnk-portfolio/',
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
-  // ADD THIS LINE FOR DEPLOYMENT TO GITHUB PAGES OR SIMILAR SUBPATH HOSTING
-   // <--- IMPORTANT: Replace 'portfolio' with your actual GitHub repository name
-});
+  build: {
+    rollupOptions: {
+      input: {
+        main: 'index.html',
+        404: 'index.html', // 👈 this creates a 404.html copy
+      }
+    }
+  }
+})
